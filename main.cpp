f9a0833bdbc6e5e1cc6f1b581772d0c7bc409163
Al1Bi2 <gggvvvv576@gmail.com>
2024-08-13 18:49:17 +0300

    mvp nvector

--- main.cpp
+++ main.cpp
@@ -3,8 +3,6 @@
 #include "ntree/nvector.h"
 int main(int argc, char** argv)
 {
-
-	std::cout << a.coords[0];
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
--- ntree/nvector.h
+++ ntree/nvector.h
@@ -1,4 +1,26 @@
 #pragma once
 
-#include <array>
+
 #include <iostream>
+#include <vector>
+template<typename T, std::size_t TDim>
+class NVector {
+public:
+	std::vector<T> coords;
+
+	NVector() : coords(TDim) {}
+	NVector<T, TDim>& operator+=(NVector<T, TDim> rhs) {
+		for (int = 0; i < TDim, i += ) {
+			coords[i] += rhs.coords[i];
+		}
+		return *this;
+	}
+
+	NVector<T, TDim>& operator*=(T rhs) {
+		for (int = 0; i < TDim, i += ) {
+			coords[i] *= rhs;
+		}
+		return *this;
+	}
+
+};
