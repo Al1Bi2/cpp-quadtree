0f468acf7423cf10b708cfab0b13ad5701ea04f6
Al1Bi2 <gggvvvv576@gmail.com>
2024-09-10 22:16:41 +0300

    continue ntree

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
-#include "ntree/nbox.h"
+#include "ntree/ntree.h"
 #include <assert.h>
 
 using namespace std;
-using namespace OrthoTree;
+
 void testBox();
+void testNode();
 int main(int argc, char** argv){
 
 
-  testBox();
+  testNode();
+  
 	return 0;
 }
 ///TODO: Z curve is one for each depth level + 1 shifted by depth * dimension.!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
@@ -18,6 +20,23 @@ int main(int argc, char** argv){
       assert(key < (MortonNodeID(1) << (depth * DIMENSION_NO)));
       return (MortonNodeID{ 1 } << (depth * DIMENSION_NO)) | key;
     } */
+
+void testNode(){
+
+  nBox<int, 2> box1(nVector<int,2>{3,3},nVector<int,2>{3,3});
+  NTree<int,2> tree;
+  tree.Init(box1,2);  
+  tree.nodes[1] = NTree<int,2>::Node(box1);
+  assert(!tree.nodes.at(1).hasChildren());
+  assert();
+}
+
+
+
+
+
+
+
 ///TODO: THIIIIIIIIIIIS
 void testBox(){
     nBox<int, 2> box1(nVector<int,2>({6,6}),nVector<int,2>{3,3});
--- ntree/ntree.h
+++ ntree/ntree.h
@@ -2,21 +2,56 @@
 
 #include "nbox.h"
 #include <unordered_map>
+#include <vector>
+#include <optional>
+using NodeID = size_t ;
 
-using NodeID = size_t;
 
-
-template< typename T, size_t dimT>
-class nTree{
+template< typename T, size_t dimT, typename dataT>
+class NTree{
 public:
-class Node{
-    public:
-    nBox box;
-    std::vector<NodeID> children;
-}
-///TODO: iplement Node aaray with z order
-private:
-    std::unordered_map<NodeID,Node> nodes;
-    nBox<T,dimT> box;
+    class Node{
+        public:
+            nBox<T,dimT> box;
+            std::vector<std::optional<NodeID>> children;
+            std::vector<dataT> content;
 
+        public:
+            Node(): box(),children(){}
+            Node( nBox<T,dimT> box):box(box), children(){}
+        public:
+            void addChild(int sub_node_id,NodeID child_id){
+                for(size_t i = 0; i< sub_node_id, i<(1<<max_depth);i++){
+                    children.at(i) = std::nullopt;
+                }
+                children.at(sub_node_id) = child_id;
+            }
+            bool hasChild(int sub_node_id){return children.at(sub_node_id).has_value();}
+            bool hasChildren(){return !children.empty();}
+            NodeID getChild(int sub_node_id){
+                return children.at(sub_node_id).value();
+            }
+            void addData(Data data){this->content.push_back(data)}
+    };
+///TODO: iplement Node aaray with z order
+//private:
+public:
+    std::unordered_map<NodeID,Node> nodes;
+    nBox<T,dimT> size;
+    size_t max_depth = 0;
+    size_t cur_depth = 0;
+    size_t cur_grid = 0;
+public:
+    NTree():nodes(),size(){}
+    void Init(nBox<T,dimT> size, size_t max_depth){
+        this->size = size;
+        this->max_depth = max_depth;
+        nodes.at(1)  = Node(size);
+
+    }
+
+public:
+    NodeID getCode(nVector<T,dimT> point){
+        
+    }
 };
